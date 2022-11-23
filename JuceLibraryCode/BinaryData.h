eb177c904c2c23abfe1e31d60ea323f8ed88b4f0
Tobias Pilarche <tobsailbot@gmail.com>
2022-11-23 19:30:18 -0300

    imgs

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -8,14 +8,26 @@
 
 namespace BinaryData
 {
+    extern const char*   emoji_cry_png;
+    const int            emoji_cry_pngSize = 55025;
+
+    extern const char*   emoji_glass_png;
+    const int            emoji_glass_pngSize = 46301;
+
+    extern const char*   emoji_laugh_png;
+    const int            emoji_laugh_pngSize = 53883;
+
+    extern const char*   emoji_normal_png;
+    const int            emoji_normal_pngSize = 50318;
+
     extern const char*   emoji_png;
-    const int            emoji_pngSize = 32064;
+    const int            emoji_pngSize = 50894;
 
     extern const char*   background_jpg;
     const int            background_jpgSize = 100726;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
-    const int namedResourceListSize = 2;
+    const int namedResourceListSize = 6;
 
     // Points to the start of a list of resource names.
     extern const char* namedResourceList[];
